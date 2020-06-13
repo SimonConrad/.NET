b0a1787c1bdc21cf52b986117ecbc26c28840da5
Szymon Kondratowicz <szymon.kondratowicz@outlook.com>
2020-06-13 17:44:53 +0200

    Add async / await

--- Orders/Orders.Web/Controllers/ProductController.cs
+++ Orders/Orders.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Orders.Web.RequestModels;
 
@@ -11,24 +12,30 @@ namespace Orders.Web.Controllers
     {
         [HttpGet]  // INFO idempotenta i bezpieczna
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
+            await Task.Delay(100);
+
             return Ok();
         }
 
         [HttpGet("{id}")] // INFO idempotenta i bezpieczna
         [ProducesResponseType(StatusCodes.Status200OK)] // INFO, Dla Swagger-a
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
+            await Task.Delay(100);
+
             return Ok();
         }
 
         [HttpPost] // INFO nieidempotenta i niebezpieczna
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult Post([FromBody] ProductReqModel productReqModel)
+        public async Task<IActionResult> Post([FromBody] ProductReqModel productReqModel)
         {
+            await Task.Delay(100);
+
             return Ok();
         }
 
@@ -36,16 +43,20 @@ namespace Orders.Web.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult Put(int id, [FromBody] ProductReqModel productReqMode)
+        public async Task<IActionResult> Put(int id, [FromBody] ProductReqModel productReqMode)
         {
+            await Task.Delay(100);
+
             return NoContent();
         }
 
         [HttpDelete("{id}")] // INFO idempotenta i niebezpieczna
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            await Task.Delay(100);
+
             return NoContent();
         }
     }
