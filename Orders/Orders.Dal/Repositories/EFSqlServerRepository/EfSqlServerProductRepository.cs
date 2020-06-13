f53855a181410aac9c46daaf4bb1ba0f89f95631
Szymon Kondratowicz <szymon.kondratowicz@outlook.com>
2020-06-13 19:37:33 +0200

    Add SQL Server EF Repository implementation

---
+++ Orders/Orders.Dal/Repositories/EFSqlServerRepository/EfSqlServerProductRepository.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Orders.Dal.Dbos;
+
+namespace Orders.Dal.Repositories.EFSqlServerRepository
+{
+    public class EfSqlServerProductRepository : IProductRepository
+    {
+        private DbContextOptionsBuilder<OrdersDbContext> optionsBuilder;
+
+        public EfSqlServerProductRepository(IConfiguration configuration)
+        {
+            optionsBuilder = new DbContextOptionsBuilder<OrdersDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("Default"));
+        }
+
+        public async ValueTask<IEnumerable<ProductDbo>> Get()
+        {
+            await using var ctxt = new OrdersDbContext(optionsBuilder.Options);
+            return await ctxt.Products.ToListAsync();
+        }
+
+        public async ValueTask<ProductDbo> Get(long id)
+        {
+            await using var ctxt = new OrdersDbContext(optionsBuilder.Options);
+            return await ctxt.Products.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async ValueTask<long> Create(ProductDbo productDbo)
+        {
+            await using var ctxt = new OrdersDbContext(optionsBuilder.Options);
+            await ctxt.Products.AddAsync(productDbo);
+
+            await ctxt.SaveChangesAsync();
+
+            return productDbo.Id;
+        }
+
+        public async ValueTask<bool> Exists(long id)
+        {
+            await using var ctxt = new OrdersDbContext(optionsBuilder.Options);
+            return await ctxt.Products.AnyAsync(x => x.Id == id);
+        }
+
+        public async ValueTask Update(long id, ProductDbo productDbo)
+        {
+            await using var ctxt = new OrdersDbContext(optionsBuilder.Options);
+            var res = await ctxt.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            res.Name = productDbo.Name;
+            res.Description = productDbo.Description;
+            res.Price = productDbo.Price;
+
+            await ctxt.SaveChangesAsync();
+        }
+
+        public async ValueTask Delete(long id)
+        {
+            await using var ctxt = new OrdersDbContext(optionsBuilder.Options);
+            var res = await ctxt.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            ctxt.Remove(res);
+
+            await ctxt.SaveChangesAsync();
+        }
+    }
+}
---
+++ Orders/Orders.Dal/Repositories/EFSqlServerRepository/OrdersDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Dal.Dbos;
+
+namespace Orders.Dal.Repositories.EFSqlServerRepository
+{
+    public class OrdersDbContext : DbContext
+    {
+        public OrdersDbContext(DbContextOptions<OrdersDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ProductDbo> Products { get; set; }
+    }
+}
