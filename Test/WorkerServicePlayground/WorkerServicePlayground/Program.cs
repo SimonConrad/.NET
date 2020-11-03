b7c8a9e1b28b3eb3b9485ba32bc7720c84c5cd65
Szymon Kondratowicz <szymon.kondratowicz@outlook.com>
2020-11-03 12:59:19 +0100

    Add INFO turning app into a windows service

--- Test/WorkerServicePlayground/WorkerServicePlayground/Program.cs
+++ Test/WorkerServicePlayground/WorkerServicePlayground/Program.cs
@@ -20,5 +20,22 @@ namespace WorkerServicePlayground
                 {
                     services.AddHostedService<Worker>();
                 });
+
+        // INFO turning app into a windows service
+        //.UseWindowsService();
+        // dotnet publish -r win-x64 -c Release
+        // To do that, first we need to publish our application.In the project directory we run :
+        
+        //dotnet publish -r win-x64 -c Release
+        //Note in my case, I’m publishing for Windows X64 which generally is going to be the case when deploying a Windows service.
+        
+        //Then all we need to do is run the standard Windows Service installer.This isn’t.NET Core specific but is instead part of Windows :
+        
+        //sc create TestService BinPath = C:\full\path\to\publish\dir\WindowsServiceExample.exe
+        //As always, the other commands available to you (including starting your service) are :
+        
+        //sc start TestService
+        //sc stop TestService
+        //sc delete TestService
     }
 }
