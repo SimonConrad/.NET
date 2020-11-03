d7187acfbf21fe3dc83efc904d145d4029e1ed20
Szymon Kondratowicz <szymon.kondratowicz@outlook.com>
2020-11-03 12:44:33 +0100

    Add test project and library project

---
+++ Test/ClassLibrary/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ClassLibrary
+{
+    public class Calculator 
+    {
+        private readonly ICalculatorDependency dependency;
+
+        public Calculator(ICalculatorDependency dependency)
+        {
+            this.dependency = dependency;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if(b == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            var res = a / b;
+            return res;
+        }
+    }
+}
---
+++ Test/NUnitTestProject/NUnitTestProject/CalculatorTests.cs
@@ -0,0 +1,57 @@
+using ClassLibrary;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace NUnitTestProject
+{
+    // TODO Wykorzytsaæ wiêcej z API NUnit-a
+    // TODO Wykorzytsaæ wiêcej z API FluentAssertions
+    // TODO Wykorzytsaæ wiêcej z API Moq
+
+    // TODO U¿yæ Bogus-a wdo mockowanie danych
+    // TODO Zapi¹æ automat do wylicania pokrycia testami kodu
+
+    // INFO Dobre praktyki: test jednostkowy testuje publiczne API, w testach trzyammy regu³ SOLID, jesli mamy wymagnie dok³adne mo¿na pisaæ TDDD, Arrange Act, Assert, testy jednostkowe troche jak BDD
+    public class CalculatorTests
+    {
+        //[SetUp]
+        //public void Setup()
+        //{
+
+        //}
+
+
+        [Test]
+        public void CalculatorTestsDivide_WhenProperArguments_ThenProperResult()
+        {
+            // Arrange
+            var dependency = new Mock<ICalculatorDependency>();
+            var calculator = new Calculator(dependency.Object);
+
+            // Act 
+            var res = calculator.Divide(2, 2);
+
+            // Assert
+            res.Should().Equals(1);
+        }
+
+        [Test]
+        public void CalculatorTestsDivide_WhenZeroInDenominator_ThenThrowArgumentException()
+        {
+            // Arrange
+            var dependency = new Mock<ICalculatorDependency>();
+            var calculator = new Calculator(dependency.Object);
+
+            // Act 
+            Action actionToTest = () =>
+            {
+                calculator.Divide(1, 0);
+            };
+
+            // Assert
+            actionToTest.Should().Throw<ArgumentException>();
+        }
+    }
+}
